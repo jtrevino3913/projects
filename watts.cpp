4de9ff207b21beeafb7ddb3893fd0cb9befd1582
Rajarshi24 <40721635+Rajarshi24@users.noreply.github.com>
2019-10-23 01:49:43 +0530

    Update watts.cpp
    User can now calculate resistance and power how many he wants! (Minimal change)

--- watts.cpp
+++ watts.cpp
@@ -13,10 +13,12 @@ double	volts = 0.0;
 double	watts = 0.0;
 double	amps = 0.0;
 double  resist = 0.0;
-
+char c='Y';
+  
 cout << "This is for finding watts and resistance \n\n";
 
-for (int i=1; i < 100; ++i){
+while(c=='y'||c=='Y')
+{
 cout << "Enter Volts: ";
 cin >> volts;
 cout << "\nEnter Amps: ";
@@ -25,6 +27,8 @@ cin >> amps;
 watts = amps * volts;
 resist = volts / amps;
 cout << "\nYour Device uses: " << watts << " Watts\n" << "and it's Resistance in ohms: " << resist << endl;
+cout<<"Do you want to calculate the Resistance and Watt for other values of Voltage and Amps? (Y/N)\n";
+cin>>c;
 }
 return 0;
 }
